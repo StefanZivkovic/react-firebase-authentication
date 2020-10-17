06963fdf29bb253239ac9587f17be0353d1de29f
Stefan Zivkovic <zivkovicstefan123@gmail.com>
2020-10-17 23:50:29 +0200

    create firebase API interface

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/auth";
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,6 +13,19 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  doCreateUserWIthEmailAndPassword = (email, password) => {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  };
+  doSIgnInWithEmailAndPassword = (email, password) => {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  };
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
