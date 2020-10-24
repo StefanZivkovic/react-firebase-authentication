57fabc2ddee6f66484c97cd58565614d067f9bd3
Stefan Zivkovic <zivkovicstefan123@gmail.com>
2020-10-24 23:13:44 +0200

    authenthicated user

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -9,23 +9,46 @@ import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
 
+import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
-const App = () => (
-  <Router>
-    <main>
-      <Navigation />
-      <hr />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authUser: null,
+    };
+  }
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged((authUser) => {
+      console.log("atuhuser", authUser);
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+  }
+  componentWillUnmount() {
+    this.listener();
+  }
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-    </main>
-  </Router>
-);
+  render() {
+    return (
+      <Router>
+        <main>
+          <hr />
+          <Navigation authUser={this.state.authUser} />
 
-export default App;
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+        </main>
+      </Router>
+    );
+  }
+}
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,39 +4,45 @@ import SignOutButton from "../SignOut";
 
 import * as ROUTES from "../../constants/routes";
 
-const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link data-testid="signin" to={ROUTES.SIGN_IN}>
-          Sign In
-        </Link>
-      </li>
-      <li>
-        <Link data-testid="landing" to={ROUTES.LANDING}>
-          Landing
-        </Link>
-      </li>
-      <li>
-        <Link data-testid="home" to={ROUTES.HOME}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link data-testid="account" to={ROUTES.ACCOUNT}>
-          Account
-        </Link>
-      </li>
-      <li>
-        <Link data-testid="admin" to={ROUTES.ADMIN}>
-          Admin
-        </Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
-  </div>
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link data-testid="landing" to={ROUTES.LANDING}>
+        Landing
+      </Link>
+    </li>
+    <li>
+      <Link data-testid="home" to={ROUTES.HOME}>
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link data-testid="account" to={ROUTES.ACCOUNT}>
+        Account
+      </Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link data-testid="signin" to={ROUTES.SIGN_IN}>
+        Sign In
+      </Link>
+    </li>
+    <li>
+      <Link data-testid="landing" to={ROUTES.LANDING}>
+        Landing
+      </Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
