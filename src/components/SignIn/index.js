e241524115b4b71090f49c5180a7dfbb334872f8
Stefan <stefi@sprintingsoftware.com>
2020-11-08 20:34:55 +0100

    link accounts

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -7,6 +7,16 @@ import {PasswordForgetLink} from '../PasswordForget';
 import {withFirebase} from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
+const ERROR_CODE_ACCOUNT_EXISTS =
+  'auth/account-exists-with-different-credential';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+  An account with an E-Mail address to
+  this social account already exists. Try to login from
+  this account instead and associate your social accounts on
+  your personal account page.
+`;
+
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
@@ -108,6 +118,9 @@ class SignInGoogleBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch((error) => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
         this.setState({error});
       });
 
@@ -150,6 +163,9 @@ class SignInFacebookBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch((error) => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
         this.setState({error});
       });
 
